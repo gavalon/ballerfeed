83cf4a83cf7d2cc320b8cea8c3865fe99fc889eb
gavalon <gavalon@stanford.edu>
2017-05-28 03:37:22 -0700

    added scraper for all players

---
+++ scripts/scrape_players.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import re, urllib
+from urllib import urlopen
+import MySQLdb
+
+
+
+def scrape_page(number,cursor,db):
+	url = 'http://basketball.realgm.com/international/stats/2017/Averages/All/All/points/All/desc/' + str(number)
+	f = urlopen(url)
+	soup = BeautifulSoup(f.read(),'lxml')
+	tbody = soup.findAll('tbody')[0]
+	links = tbody.findAll('a')
+	for link in links:
+		id_ = re.findall('Summary/(.+?)">',str(link))[0]
+		name = link.contents[0]
+		query = "INSERT IGNORE INTO players (player_id, player_name) VALUES (" + id_ + ',"' +  name + '"' + ");"
+		cursor.execute(query)
+		db.commit()
+	
+
+
+if __name__ == "__main__":
+	db = MySQLdb.connect("localhost","root","","ballusers")
+	cursor = db.cursor()
+	total_pages = 132
+	for i in range(1,total_pages + 1):
+		print i
+		scrape_page(i,cursor,db)
+	db.close()
